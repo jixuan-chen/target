c660a49ba4183ae21dc5b8db415dc4b52c4f5067
Jixuan Chen <veroyoonmn@gmail.com>
2024-10-14 12:36:02 +0200

    restore package structure

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .example import generate_example
-from .scripts import Target
--- scripts/SfcRi.py
+++
@@ -1,5 +0,0 @@
-def sfc_ri(dz, Thi, Tlo, Uhi):
-    Tcorrhi = Thi + 9.806 / 1004.67 * dz
-    Ri = 9.806 * dz * (Tcorrhi - Tlo) * 2. / (Thi + Tlo) / (Uhi ** 2)
-
-    return {'Ri': Ri, 'Tcorrhi': Tcorrhi}
